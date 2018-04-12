b17ffc639e4d229f3cc23365d68e0b4253e52529
sugata <sugatabhar@yahoo.com>
2018-04-12 19:14:30 +0530

    Added captcha

--- index.php
+++ index.php
@@ -4,14 +4,21 @@ header('Access-Control-Allow-Origin: *');
 $values = json_decode(file_get_contents("msg.json"), true);	
 
 if( !empty( $_POST ) ){
-	$postarray = array(
-	  "name"    =>     $_POST['name'],
-	  "comment" =>     $_POST['comment'],
-	  "timer"    =>     $_POST['timer']
-	  
-	);
-	array_push($values,$postarray);
-	file_put_contents("msg.json", json_encode($values));
+	session_start();
+	if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"]) {
+		echo "Correct Code Entered";
+		//Do you stuff
+		$postarray = array(
+		  "name"    =>     $_POST['name'],
+		  "comment" =>     $_POST['comment'],
+		  "timer"    =>     $_POST['timer']
+		  
+		);
+		array_push($values,$postarray);
+		file_put_contents("msg.json", json_encode($values));
+	}else{
+		die("Wrong Code Entered");
+	}
 }
 
 $values = json_decode(file_get_contents("msg.json"), true);	    
