ef7632824be7d55631d74696ab9eb3af09039ff0
sugata <sugatabhar@yahoo.com>
2018-04-13 11:44:55 +0530

    Update json file when $code equals captcha

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ imagedestroy($im);
 $values = json_decode(file_get_contents("msg.json"), true);	
 
 if( !empty( $_POST ) ){
-	
-	if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"]) {
+	echo $_POST["captcha"];
+	if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$code==$_POST["captcha"]) {
 		echo "Correct Code Entered";
 		//Do you stuff
 		$postarray = array(
