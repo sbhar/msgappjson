af5694181871cbefb4aa5a24dd551013a4317a8a
sugata <sugatabhar@yahoo.com>
2018-04-13 12:04:11 +0530

    Checking whether works from same server

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
-//session_start();
-$code=rand(1000,9999);
-//$_SESSION["code"]=$code;
-$im = imagecreatetruecolor(50, 24);
-$bg = imagecolorallocate($im, 22, 86, 165); //background color blue
-$fg = imagecolorallocate($im, 255, 255, 255);//text color white
-imagefill($im, 0, 0, $bg);
-imagestring($im, 5, 5, 5,  $code, $fg);
+session_start();
 header('Access-Control-Allow-Origin: *');
-header("Cache-Control: no-cache, must-revalidate");
-header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
-
 	
 $values = json_decode(file_get_contents("msg.json"), true);	
 
 if( !empty( $_POST ) ){
-//if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"]) {
+	echo $_POST["captcha"];
 	if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$code==$_POST["captcha"]) {
 		echo "Correct Code Entered";
 		//Do you stuff
